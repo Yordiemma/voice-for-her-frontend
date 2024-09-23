6d5c629826f937ab364a717daf9b3b0a9ee8a6f6
Yordiemma <yordisweet56@gmail.com>
2024-09-23 11:45:19 +0200

    router debugging

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-import React from 'react';
-import './App.css'; // Import the global styles
-
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import About from './components/About';
 import Help from './components/Help';
@@ -9,20 +6,12 @@ import Help from './components/Help';
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Routes>
-          {/* Redirect root path to /home */}
-          <Route path="/" element={<Navigate to="/home" />} />
-
-          {/* Define actual home route */}
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/help" element={<Help />} />
-
-          {/* Optional: Catch-all route for unknown paths */}
-          <Route path="*" element={<Navigate to="/home" />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        
+        <Route path="/about" element={<About />} />
+        <Route path="/help" element={<Help />} />
+      </Routes>
     </Router>
   );
 }
